ee0870641b0a66b5d1a107f840dd03c7e52501ca
Faishal797 <faishalgm97@gmail.com>
2023-10-24 00:46:34 +0700

    Try Intent

--- app/src/main/java/com/zhalz/grato/ui/main/MainActivity.kt
+++ app/src/main/java/com/zhalz/grato/ui/main/MainActivity.kt
@@ -18,5 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
+        binding.context = this
     }
 }
--- app/src/main/java/com/zhalz/grato/ui/main/MainViewModel.kt
+++ app/src/main/java/com/zhalz/grato/ui/main/MainViewModel.kt
@@ -1,7 +1,10 @@
 package com.zhalz.grato.ui.main
 
+import android.content.Context
+import android.content.Intent
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import com.zhalz.grato.ui.MainActivity2
 
 class MainViewModel : ViewModel() {
     var counter = ObservableField(0)
@@ -14,5 +17,10 @@ class MainViewModel : ViewModel() {
     fun decreaseCounter() {
         val currentCounter = counter.get() ?: 0
         counter.set(currentCounter - 1)
+    }
+
+    fun navigateToOtherActivity(context: Context) {
+        val intent = Intent(context, MainActivity2::class.java)
+        context.startActivity(intent)
     }
 }
