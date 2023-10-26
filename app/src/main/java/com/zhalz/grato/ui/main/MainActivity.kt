df233929c5c54ffc70676c5633a3429f2830fc2c
Faishal797 <faishalgm97@gmail.com>
2023-10-26 18:37:00 +0700

    Menerapkan SharedPreference tanpa Dependency Injection

--- app/src/main/java/com/zhalz/grato/ui/main/MainActivity.kt
+++ app/src/main/java/com/zhalz/grato/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.zhalz.grato.R
+import com.zhalz.grato.data.CounterSP
 import com.zhalz.grato.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels{
+        MainViewModel.Factory(CounterSP(this))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/zhalz/grato/ui/main/MainViewModel.kt
+++ app/src/main/java/com/zhalz/grato/ui/main/MainViewModel.kt
@@ -2,18 +2,28 @@ package com.zhalz.grato.ui.main
 
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.zhalz.grato.data.CounterSP
 
-class MainViewModel : ViewModel() {
-    var counter = ObservableField(0)
+class MainViewModel(private val counterSP: CounterSP) : ViewModel() {
+    var counter = ObservableField(counterSP.counter)
 
     fun increaseCounter() {
-        val currentCounter = counter.get() ?: 0
-        counter.set(currentCounter + 1)
+//        val currentCounter = counter.get() ?: 0
+        counterSP.counter += 1
+        counter.set(counterSP.counter)
     }
 
     fun decreaseCounter() {
-        val currentCounter = counter.get() ?: 0
-        counter.set(currentCounter - 1)
+//        val currentCounter = counter.get() ?: 0
+        counterSP.counter -= 1
+        counter.set(counterSP.counter)
+    }
+
+    class Factory(private val counterSP: CounterSP): ViewModelProvider.Factory{
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MainViewModel(counterSP) as T
+        }
     }
 
 }
