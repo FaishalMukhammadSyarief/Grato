23d92064fda8a1080f9227e27296d4f8695801fe
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-12-09 18:20:36 +0700

    Implement crocodic core on code

--- app/src/main/java/com/zhalz/grato/ui/main/MainActivity.kt
+++ app/src/main/java/com/zhalz/grato/ui/main/MainActivity.kt
@@ -1,24 +1,18 @@
 package com.zhalz.grato.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
 import com.zhalz.grato.R
+import com.zhalz.grato.base.BaseActivity
 import com.zhalz.grato.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private val binding: ActivityMainBinding by lazy {
-        DataBindingUtil.setContentView(this, R.layout.activity_main)
-    }
-
-    private val viewModel: MainViewModel by viewModels()
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding.viewModel = viewModel
+
     }
 }
--- app/src/main/java/com/zhalz/grato/ui/main/MainViewModel.kt
+++ app/src/main/java/com/zhalz/grato/ui/main/MainViewModel.kt
@@ -1,23 +1,22 @@
 package com.zhalz.grato.ui.main
 
 import androidx.databinding.ObservableField
-import androidx.lifecycle.ViewModel
+import com.zhalz.grato.base.BaseViewModel
 import com.zhalz.grato.data.CounterSP
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val counterSP: CounterSP) : ViewModel() {
+class MainViewModel @Inject constructor(private val counterSP: CounterSP) : BaseViewModel() {
+
     var counter = ObservableField(counterSP.counter)
 
     fun increaseCounter() {
-//        val currentCounter = counter.get() ?: 0
         counterSP.counter += 1
         counter.set(counterSP.counter)
     }
 
     fun decreaseCounter() {
-//        val currentCounter = counter.get() ?: 0
         counterSP.counter -= 1
         counter.set(counterSP.counter)
     }
